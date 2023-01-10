5c36ae5f367176575cb5a08dbd4bb06845c0b729
Maicou Hahn <maicouhahn11@gmail.com>
2023-01-10 19:47:29 -0300

    Criados DAO Factory e SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,8 @@ public class Program {
 		Department d1 = new Department(1, "Books");
 
 		Seller s1 = new Seller(21, "Bob","bob@gmail.com", new Date(), 3000.00, d1);
+		
+		SellerDAO sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(s1);
 
