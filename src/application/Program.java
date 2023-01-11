22b3ffab95bbc40c247d8b796d15271241f1cc69
Maicou Hahn <maicouhahn11@gmail.com>
2023-01-11 10:40:36 -0300

    Implementacao do metodo Seller FindAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,11 +16,20 @@ public class Program {
 		System.out.println("===Test 1: seller findbyId ====");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
+		
 		System.out.println();
-		System.out.println("===Test 1: seller findbyDepartment ====");
+		
+		System.out.println("===Test 2: seller findbyDepartment ====");
 		Department department = new Department(2,null);
 		List<Seller> list = sellerDao.findByDepartment(department);
+		for(Seller s:list) {
+			System.out.println(s);
+		}
 		
+		System.out.println();
+		
+		System.out.println("===Test 3: seller findAll ====");
+		list = sellerDao.findAll();
 		for(Seller s:list) {
 			System.out.println(s);
 		}
