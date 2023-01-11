7f15c19e5b6a78895f7fc8b2ebf2a5a7e347c023
Maicou Hahn <maicouhahn11@gmail.com>
2023-01-11 09:10:50 -0300

    Implementacao do metodo SellerFindById()

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,13 +11,12 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Department d1 = new Department(1, "Books");
 
-		Seller s1 = new Seller(21, "Bob","bob@gmail.com", new Date(), 3000.00, d1);
-		
 		SellerDAO sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println(s1);
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 
 	}
 
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,34 +1,86 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDAO;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDAO {
 
+	private Connection conn; //a conexão permanece aberta nessa classe e é fechada apenas na aplicaçao, pois podera ser
+	//usada em outras tarefas da classe
+
+	public SellerDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
+
 	@Override
 	public void insert(Seller obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void update(Seller obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteById(Integer id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public Seller findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement("select seller.*,department.Name as DepName "
+					+ "from seller inner join department "
+					+ "on seller.DepartmentId = department.Id "
+					+ "where seller.Id = ?");
+			st.setInt(1,id);
+			rs = st.executeQuery();// a query vai ser executada e o resultado da execução sera armazenado pelo objeto ResultSet
+			
+			//lembrando que o resulset mostra os dados em forma de tabela e que a primeira posiçao dele é a posiçao 0, que nao há informaçoes de fato
+			//entao testa-se a posiçao, se ele for igual a 0 quer dizer que nao há elementos resultantes da pesquisa da query
+			
+			if(rs.next()) {
+				
+				Department dep = new Department();
+				dep.setId(rs.getInt("DepartmentId"));
+				dep.setName(rs.getString("DepName"));
+				
+				Seller obj = new Seller();
+				obj.setId(rs.getInt("Id"));
+				obj.setName(rs.getString("Name"));
+				obj.setEmail(rs.getString("Email"));
+				obj.setBaseSalary(rs.getDouble("BaseSalary"));
+				obj.setBirthDate(rs.getDate("BirthDate"));
+				obj.setDepartment(dep);
+				
+				return obj;
+			}
+			
+			return null;
+			
+		}catch(SQLException e) {
+			throw new DbException("Erro: "+e.getMessage());
+		}finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+		
 	}
 
 	@Override
